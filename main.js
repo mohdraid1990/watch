52f1876811228e5d0b139c95eb439253c860b678
mohdraid1990 <105244091+mohdraid1990@users.noreply.github.com>
2023-10-27 16:25:53 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   let counter = document.getElementById('counter');
-  let previousVisits = parseInt(getCookie('visits')) || 0;
-  let currentVisits = previousVisits + 1;
-  let totalVisits = parseInt(getCookie('totalVisits')) || 0;
-  totalVisits += currentVisits - previousVisits;
-  setCookie('visits', currentVisits.toString(), 365);
-  setCookie('totalVisits', totalVisits.toString(), 365);
-  counter.textContent = `المشاهدات الحالية: ${currentVisits} - المشاهدات السابقة: ${previousVisits} - إجمالي المشاهدات: ${totalVisits}`;
-});
-
-function getCookie(name) {
-  let value = "; " + document.cookie;
-  let parts = value.split("; " + name + "=");
-  if (parts.length === 2) return parts.pop().split(";").shift();
-}
-
-function setCookie(name, value, days) {
-  let expires = "";
-  if (days) {
-    let date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = "; expires=" + date.toUTCString();
+  let visits = localStorage.getItem('visits');
+  
+  if (!visits) {
+    visits = 1;
+    localStorage.setItem('visits', visits);
+  } else {
+    visits = parseInt(visits) + 1;
+    localStorage.setItem('visits', visits);
   }
-  document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
+  
+  counter.textContent = `عدد المشاهدات: ${visits}`;
+});
